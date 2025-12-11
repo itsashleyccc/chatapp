0189ca625e4519d818c21a6337b33a19e651a89c
ashley <ashleychen717@gmail.com>
2025-12-11 16:47:39 +0800

    fix host port for render

--- main.py
+++ main.py
@@ -94,5 +94,7 @@ def disconnect():
 
     send({"name": name, "avatar": "💨", "message": "left the room"}, to=room)
 
+import os
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
