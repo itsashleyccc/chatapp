833ecae93bb1ba89ef8c04ad028e1d0078c613cb
ashley <ashleychen717@gmail.com>
2025-12-11 16:27:44 +0800

    fix typo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ffrom flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
